0d7f18ecef8f64ed9a4d8b8f14e15415827f02b9
Nikhil Ahire <nikhilahire16@gmail.com>
2022-12-18 18:15:48 +0000

    Update db.php

--- app/db.php
+++ app/db.php
@@ -1,6 +1,14 @@
 <?php
+    $cleardb_url = parse_url(getenv("mysql://b8239eb638aaff:9ea7a2ac@us-cdbr-east-06.cleardb.net/heroku_3aaeb78f9450d07?reconnect=true"));
+    $cleardb_server = $cleardb_url["us-cdbr-east-06.cleardb.net"];
+    $cleardb_username = $cleardb_url["b8239eb638aaff"];
+    $cleardb_password = $cleardb_url["9ea7a2ac"];
+    $cleardb_db = substr($cleardb_url["mysql://b8239eb638aaff:9ea7a2ac@us-cdbr-east-06.cleardb.net/heroku_3aaeb78f9450d07?reconnect=true"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
 
-$connection = mysqli_connect('localhost', 'root', '', 'ecommerce');
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 if(!$connection) {
     die("Database connection failed");
 }
